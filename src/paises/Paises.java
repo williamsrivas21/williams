2bc0372517e5b11cc1fad8e6d94d5cbc90825e79
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 14:07:32 -0500

    cuarto Comit para repositor remoto =

---
+++ src/paises/Paises.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paises;
+
+/**
+ *
+ * @author WILLIAMS
+ */
+public class Paises {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String[] grupo = new String[21];
+     
+   grupo[0] = "Nirvana";
+   grupo[1] = "Reik";
+   grupo[2] = "Guns & Roses";
+   grupo[3] = "Aerosmith";
+      
+    }
+    
+}
